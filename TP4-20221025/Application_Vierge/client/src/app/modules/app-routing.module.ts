c93ab2920c817706e3498761ae7570c0ccb26a97
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2022-12-04 00:36:53 -0500

    fixed duplicated ui and routes

--- TP4-20221025/Application_Vierge/client/src/app/modules/app-routing.module.ts
+++ TP4-20221025/Application_Vierge/client/src/app/modules/app-routing.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-
-import { AppComponent } from "../app.component";
 import { WelcomePageComponent } from "../pages/welcome-page/welcome-page.component";
 import { PlanRepasTableComponent } from "../plan-repas-table/plan-repas-table.component";
 
 const routes: Routes = [
-  {
-    path: "app", component: AppComponent,
-  },
+  { path: "", redirectTo: "welcome", pathMatch: "full" },
   {
     path: "welcome", component: WelcomePageComponent
   },
   {
     path: "planrepas", component: PlanRepasTableComponent
   },
-  { path: '**', redirectTo: 'app' }
 ];
 
 @NgModule({
