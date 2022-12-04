c8846bd565d3490c1a78cde86525f733fc145d0e
rachadchazbek17 <rachadchazbek@gmail.com>
2022-12-04 09:30:06 -0500

    Added planrepas features

--- TP4-20221025/Application_Vierge/client/src/app/plan-repas-table/plan-repas-table.component.ts
+++ TP4-20221025/Application_Vierge/client/src/app/plan-repas-table/plan-repas-table.component.ts
@@ -15,47 +15,47 @@ const DELETE_CONFIRMATION_MESSAGE = 'Est ce que vous êtes sure de supprimer?';
   styleUrls: ['./plan-repas-table.component.css']
 })
 export class PlanRepasTableComponent implements OnInit {
-  
+
   constructor(private readonly controller: ClientControllerService, public dialog: MatDialog) { }
-  displayedColumns: string[] = ['numeroplan','categorie','frequence','nbrcalories','nbrpersonnes','numerofournisseur','prix','action'];
+  displayedColumns: string[] = ['numeroplan', 'categorie', 'frequence', 'nbrcalories', 'nbrpersonnes', 'numerofournisseur', 'prix', 'action'];
   allPlanRepas: PlanRepas[] = [];
   ngOnInit(): void {
-    try{
-    this.controller.getAllPlanRepas().subscribe((allPlanRepas)=>{
-      this.allPlanRepas = allPlanRepas;
-      console.log(this.allPlanRepas);
-    })
+    try {
+      this.controller.getAllPlanRepas().subscribe((allPlanRepas) => {
+        this.allPlanRepas = allPlanRepas;
+        console.log(this.allPlanRepas);
+      })
     }
-    catch{}
+    catch { }
   }
-  add(planRepas: PlanRepas)
-  {
-    try{
+  add(planRepas: PlanRepas) {
+    try {
       this.dialog.open(AddDialogComponent, {
         width: '350px',
       });
       this.controller.addPlanrepas(planRepas);
+      this.ngOnInit();
     }
-    catch{}
+
+    catch { }
   }
-  update(numeroplan: string)
-  {
-    try{
+  update(numeroplan: string) {
+    try {
       let updateDialog = this.dialog.open(UpdateDialogComponent, {
         width: '350px',
-        data: {numeroplan: numeroplan}
+        data: { numeroplan: numeroplan }
       });
-      updateDialog.afterClosed().subscribe((result)=>{alert(result)});
+      updateDialog.afterClosed().subscribe((result) => { alert(result) });
     }
-    catch{}
+    catch { }
+    this.ngOnInit();
   }
-  delete(numeroplan: string)
-  {
-    try{
+  delete(numeroplan: string) {
+    try {
       const deleteDialog = this.dialog.open(DialogComponent, {
-        data:{
+        data: {
           numeroplan: numeroplan,
-          message: DELETE_CONFIRMATION_MESSAGE ,
+          message: DELETE_CONFIRMATION_MESSAGE,
           buttonText: {
             ok: ' Oui',
             cancel: 'Non'
@@ -64,12 +64,13 @@ export class PlanRepasTableComponent implements OnInit {
         width: '350px'
       });
       deleteDialog.afterClosed().subscribe((confirmed: boolean) => {
-        if(confirmed){
-        this.controller.deletePlanrepas(numeroplan);
+        if (confirmed) {
+          this.controller.deletePlanrepas(numeroplan);
+          this.ngOnInit();
         }
       });
-     
+
     }
-    catch{}
+    catch { }
   }
 }
